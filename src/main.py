4ee005507aa42b6ae412a9b9eada46db276cca05
Nurlis-Abdymalikov <Nurlisabdymalikov0@gmail.com>
2025-04-10 23:00:53 +0600

    hw_2

--- src/main.py
+++ src/main.py
@@ -1,23 +1,38 @@
+# import flet as ft
+
+# def main(page: ft.Page):
+#     page.title = "Hi Word"
+
+#     def change_name(e):
+#         print(name_input.value)
+
+#     def click_button(e):
+#         print(name_input.value)
+    
+#     name_input = ft.TextField(
+#         label='enter your name',
+#         on_change=change_name
+#     )
+
+#     button = ft.ElevatedButton('save', on_click=click_button)
+
+
+#     page.add(name_input, button)
+
+
+# ft.app(main) 
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "Hi Word"
-
-    def change_name(e):
-        print(name_input.value)
-
-    def click_button(e):
-        print(name_input.value)
-    
+    friends = ["Atay","Tanay","Amir","Marsel"]
+    def on_change(e):
+        input_text = e.control.value.strip()
+        if input_text in friends:
+            print(f'{input_text}')
     name_input = ft.TextField(
-        label='enter your name',
-        on_change=change_name
+        label = "Введите имя",
+        on_change=on_change,
     )
+    page.add(name_input)
 
-    button = ft.ElevatedButton('save', on_click=click_button)
-
-
-    page.add(name_input, button)
-
-
-ft.app(main)
+ft.app(target=main)
