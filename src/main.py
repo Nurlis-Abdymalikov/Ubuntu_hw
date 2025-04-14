4256cf2f8d8cb852717d45e9651e5dd4f22d6f98
Nurlis-Abdymalikov <Nurlisabdymalikov0@gmail.com>
2025-04-14 22:36:14 +0600

    hw_3

--- src/main.py
+++ src/main.py
@@ -1,38 +1,29 @@
-# import flet as ft
-
-# def main(page: ft.Page):
-#     page.title = "Hi Word"
-
-#     def change_name(e):
-#         print(name_input.value)
-
-#     def click_button(e):
-#         print(name_input.value)
-    
-#     name_input = ft.TextField(
-#         label='enter your name',
-#         on_change=change_name
-#     )
-
-#     button = ft.ElevatedButton('save', on_click=click_button)
-
-
-#     page.add(name_input, button)
-
-
-# ft.app(main) 
 import flet as ft
 
 def main(page: ft.Page):
-    friends = ["Atay","Tanay","Amir","Marsel"]
-    def on_change(e):
-        input_text = e.control.value.strip()
-        if input_text in friends:
-            print(f'{input_text}')
-    name_input = ft.TextField(
-        label = "Введите имя",
-        on_change=on_change,
-    )
-    page.add(name_input)
+    # title = ft.Text("To-do list", size=30, weight=ft.FontWeight.BOLD)
+    page.title = "Hello World"
 
-ft.app(target=main)
+    def change_name(e):
+        print(name_input.value)
+
+    def click_button(e):
+        print(name_input.value)
+
+    
+    name_input = ft.TextField(
+        label='enter your name',
+        on_change=change_name
+    )
+
+    def listt(e):
+        list = name_input.value
+
+    list = ft.Text(list)
+
+    button = ft.ElevatedButton('save', on_click=click_button)
+    
+
+    page.add(name_input, button,list)
+
+ft.app(main) 
