65cdf132c11e36512c223882d661a1ff1f52ff64
Nurlis-Abdymalikov <Nurlisabdymalikov0@gmail.com>
2025-04-17 22:31:11 +0600

    hw4

--- src/main.py
+++ src/main.py
@@ -1,29 +1,37 @@
 import flet as ft
 
 def main(page: ft.Page):
-    # title = ft.Text("To-do list", size=30, weight=ft.FontWeight.BOLD)
-    page.title = "Hello World"
+    page.title = "Трекер расходов"
+    expenses = []
+    name_input = ft.TextField(label="Описание расхода", width=300)
+    amount_input = ft.TextField(label="Сумма", width=150)
+    total_text = ft.Text(value="0", size=20, weight="bold", color=ft.colors.BLUE)
+    expense_list = ft.Column()
 
-    def change_name(e):
-        print(name_input.value)
+    def add_expense(e):
+        name = name_input.value
+        amount_str = amount_input.value
+        try:
+            amount = float(amount_str)
+            if amount <= 0:
+                raise ValueError()
+        except ValueError:
+            page.snack_bar = ft.SnackBar(ft.Text("Сумма должна быть положительным числом!"))
+            page.snack_bar.open = True
+            page.update()
+            return
+        expense_list.controls.append(ft.Text(f"{name}: {amount} сом"))
+        current_total = float(total_text.value)
+        total_text.value = str(current_total + amount)
+        name_input.value = ""
+        amount_input.value = ""
+        page.update()
 
-    def click_button(e):
-        print(name_input.value)
-
-    
-    name_input = ft.TextField(
-        label='enter your name',
-        on_change=change_name
+    page.add(
+        ft.Row([name_input, amount_input, ft.ElevatedButton(text="Сохранить", on_click=add_expense)]),
+        expense_list,
+        ft.Text("Общая сумма расходов:", size=16),
+        total_text
     )
 
-    def listt(e):
-        list = name_input.value
-
-    list = ft.Text(list)
-
-    button = ft.ElevatedButton('save', on_click=click_button)
-    
-
-    page.add(name_input, button,list)
-
-ft.app(main) 
+ft.app(target=main)
