c4799abb6c3e2f6278be4c5015c818c3db4683fd
Nurlis-Abdymalikov <Nurlisabdymalikov0@gmail.com>
2025-04-21 16:34:10 +0600

    againhw3

--- src/main.py
+++ src/main.py
@@ -1,38 +1,26 @@
 import flet as ft
-
 def main(page: ft.Page):
-    page.title = "Трекер расходов"
-    expenses = []
-    name_input = ft.TextField(label="Описание расхода", width=300)
-    amount_input = ft.TextField(label="Сумма", width=150)
-    total_text = ft.Text(value="0", size=20, weight="bold", color=ft.colors.BLUE)
-    expense_list = ft.Column()
-
-    def add_expense(e):
-        name = name_input.value
-        amount_str = amount_input.value
+    page.title = "Список друзей"
+    name_field = ft.TextField(label='Имя') 
+    age_field = ft.TextField(label='Возраст', keyboard_type=ft.KeyboardType.NUMBER)
+    friends = []
+    def add_friend(e):
+        name = name_field.value.strip()
         try:
-            amount = float(amount_str)
-            if amount <= 0:
-                raise ValueError()
+            age = int(age_field.value)
         except ValueError:
-            page.snack_bar = ft.SnackBar(ft.Text("Сумма должна быть положительным числом!"))
-            page.snack_bar.open = True
-            page.update()
-            return
-        expense_list.controls.append(ft.Text(f"{name}: {amount} сом"))
-        current_total = float(total_text.value)
-        total_text.value = str(current_total + amount)
-        name_input.value = ""
-        amount_input.value = ""
+            age = 0
+        friend = {"name": name, "age": age}
+        friends.append(friend)
+        print("Список друзей")
+        print(friends)
+        name_field.value = ""
+        age_field.value = ""
         page.update()
-
+ 
     page.add(
-        ft.Row([name_input, amount_input, ft.ElevatedButton(text="Сохранить", on_click=add_expense)]),
-        expense_list,
-        ft.Text("Общая сумма расходов:", size=16),
-        total_text
+        name_field,
+        age_field,
+        ft.ElevatedButton(text="Добавить друга", on_click=add_friend),
     )
-
 ft.app(target=main)
-print('hello')
